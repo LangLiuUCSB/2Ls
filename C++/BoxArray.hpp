65285f30e61ef20bbffef24c8554017ca22aedfe
LangLiuUCSB <157067894+LangLiuUCSB@users.noreply.github.com>
2024-11-30 00:00:43 -0800

    Update BoxArray.hpp

--- C++/BoxArray.hpp
+++ C++/BoxArray.hpp
@@ -2,45 +2,53 @@
 #define BOXARRAY_HPP
 
 #include <stddef.h>
+#include <iterator>
+#include <stdexcept>
 
 namespace _2Ls
 {
     template <typename T>
     class BoxArray
     {
+    protected:
         T *_data;
         size_t _max_size, _end = 0;
 
     public:
         BoxArray(const size_t &size) noexcept
-            : _data(new T[size]), _max_size(size) {}              // Parameterized constructor
+            : _data(new T[size]), _max_size(size) {} // Parameterized constructor
+        BoxArray(std::initializer_list<T> init)
+            : _data(new T[init.size()]), _max_size(init.size()), _end(init.size())
+        {
+            std::copy(init.begin(), init.end(), _data.get());
+        }
         BoxArray() : _data(nullptr), _max_size(0) {}              // Default constructor
         BoxArray(const BoxArray &) noexcept = default;            // Copy constructor
         BoxArray(BoxArray &&) noexcept = default;                 // Move constructor
         BoxArray &operator=(const BoxArray &) noexcept = default; // Copy assignment
         BoxArray &operator=(BoxArray &&) noexcept = default;      // Move assignment
-        ~BoxArray() { delete[] _data; };                          // Default destructor
+        virtual ~BoxArray() { delete[] _data; }                   // Default destructor
 
         virtual T *begin() noexcept { return _data; }
         virtual const T *begin() const noexcept { return _data; }
-        T *end() noexcept { return _data + _end; }
-        const T *end() const noexcept { return _data + _end; }
-        std::reverse_iterator<T *> rbegin() noexcept { return std::reverse_iterator<T *>(end()); }
-        std::reverse_iterator<const T *> rbegin() const noexcept { return std::reverse_iterator<const T *>(end()); }
-        std::reverse_iterator<T *> rend() noexcept { return std::reverse_iterator<T *>(begin()); }
-        std::reverse_iterator<const T *> rend() const noexcept { return std::reverse_iterator<const T *>(begin()); }
-        const T *cbegin() const noexcept { return begin(); }
-        const T *cend() const noexcept { return end(); }
-        std::reverse_iterator<const T *> crbegin() const noexcept { return rbegin(); }
-        std::reverse_iterator<const T *> crend() const noexcept { return rend(); }
+        virtual T *end() noexcept { return _data + _end; }
+        virtual const T *end() const noexcept { return _data + _end; }
+        virtual std::reverse_iterator<T *> rbegin() noexcept { return std::reverse_iterator<T *>(end()); }
+        virtual std::reverse_iterator<const T *> rbegin() const noexcept { return std::reverse_iterator<const T *>(end()); }
+        virtual std::reverse_iterator<T *> rend() noexcept { return std::reverse_iterator<T *>(begin()); }
+        virtual std::reverse_iterator<const T *> rend() const noexcept { return std::reverse_iterator<const T *>(begin()); }
+        virtual const T *cbegin() const noexcept { return begin(); }
+        virtual const T *cend() const noexcept { return end(); }
+        virtual std::reverse_iterator<const T *> crbegin() const noexcept { return rbegin(); }
+        virtual std::reverse_iterator<const T *> crend() const noexcept { return rend(); }
 
         virtual size_t size() const noexcept { return _end; }
-        size_t max_end() const noexcept { return _max_size; }
+        virtual size_t max_end() const noexcept { return _max_size; }
         virtual bool empty() const noexcept { return _end == 0; }
-        bool full() const noexcept { return _end == _max_size; }
+        virtual bool full() const noexcept { return _end == _max_size; }
 
-        T &operator[](const size_t &index) noexcept { return _data[index]; }
-        const T &operator[](const size_t &index) const noexcept { return _data[index]; }
+        virtual T &operator[](const size_t &index) noexcept { return _data[index]; }
+        virtual const T &operator[](const size_t &index) const noexcept { return _data[index]; }
         virtual T &at(const size_t &index)
         {
             if (index >= _end)
@@ -53,12 +61,32 @@ namespace _2Ls
                 throw std::out_of_range("_2Ls::BoxArray::at out of range");
             return _data[index];
         }
-        virtual T &front() noexcept { return _data[0]; }
-        virtual const T &front() const noexcept { return _data[0]; }
-        T &back() noexcept { return _data[_end - 1]; }
-        const T &back() const noexcept { return _data[_end - 1]; }
-        T *data() noexcept { return _data; }
-        const T *data() const noexcept { return _data; }
+        virtual T &front()
+        {
+            if (_max_size == 0)
+                throw std::out_of_range("_2Ls::BoxArray::front out of range");
+            return _data[0];
+        }
+        virtual const T &front() const
+        {
+            if (_max_size == 0)
+                throw std::out_of_range("_2Ls::BoxArray::front out of range");
+            return _data[0];
+        }
+        virtual T &back()
+        {
+            if (_max_size == 0)
+                throw std::out_of_range("_2Ls::BoxArray::back out of range");
+            return _data[_end - 1];
+        }
+        virtual const T &back() const
+        {
+            if (_max_size == 0)
+                throw std::out_of_range("_2Ls::BoxArray::back out of range");
+            return _data[_end - 1];
+        }
+        virtual T *data() noexcept { return _data; }
+        virtual const T *data() const noexcept { return _data; }
 
         virtual void push(const T &value)
         {
@@ -73,11 +101,12 @@ namespace _2Ls
             --_end;
         }
 
-        T &extract() = 0;
-        const T &extract() = 0;
-
-        template <typename... Args>
-        void emplace(Args &&...args) = 0;
+        virtual T &extract()
+        {
+            if (_end == 0)
+                throw std::underflow_error("_2Ls::BoxArray::extract_top underflow");
+            return _data[--_end];
+        }
     };
 }
 
